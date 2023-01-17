3581d0ef01ed1bdd54709292558fa63880f9c1ee
jollyroger-jelly <jonrizk@protonmail.com>
2023-01-17 00:22:09 -0600

    wip: implemented initial building of the spreadsheet file

--- on_guild_ready.go
+++ on_guild_ready.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/disgoorg/disgo/events"
 	"github.com/disgoorg/log"
+	"github.com/jackc/pgx/v5"
 	"google.golang.org/api/sheets/v4"
 )
 
@@ -15,6 +16,7 @@ func buildFile(countIsGreaterThan0 bool) {
 		log.Fatal(err)
 		panic(err)
 	}
+	log.Debug("file created")
 	columnMap, err := NewColumnMap(botConfig.MountSpreadsheetColumnDataFilepath)
 	if err != nil {
 		log.Fatal(err)
@@ -32,12 +34,21 @@ func buildFile(countIsGreaterThan0 bool) {
 		log.Fatal(err)
 		panic(err)
 	}
+	permIDs := make([]PermissionID, len(perms))
 	for i := 0; i < len(perms); i++ {
-		_, err = addFilePermmission(*fileID, perms[i])
+		pid, err := addFilePermmission(*fileID, perms[i])
 		if err != nil {
 			log.Fatal(err)
 			panic(err)
 		}
+		permIDs[i] = *pid
+		log.Debug(
+			"permission added for: id=%s;email=%s;role=%s;type=%s",
+			permIDs[i],
+			perms[i].EmailAddress,
+			perms[i].Role,
+			perms[i].Type,
+		)
 	}
 
 	spreadsheet, err := gsheetsSvc.Spreadsheets.Get(string(*fileID)).Do()
@@ -67,6 +78,7 @@ func buildFile(countIsGreaterThan0 bool) {
 		log.Fatal(err)
 		panic(err)
 	}
+	log.Debug("sheets created")
 	// map expansions to sheet IDs
 	spreadsheet, err = gsheetsSvc.Spreadsheets.Get(spreadsheet.SpreadsheetId).Do()
 	if err != nil {
@@ -110,6 +122,7 @@ func buildFile(countIsGreaterThan0 bool) {
 						Values: cellData,
 					},
 				},
+				SheetId: int64(sheetMap[Expansion(sheetIndex)]),
 			},
 		})
 		requests = append(requests, &sheets.Request{
@@ -146,33 +159,157 @@ func buildFile(countIsGreaterThan0 bool) {
 		log.Fatal(err)
 		panic(err)
 	}
+	log.Debug("header rows and protected ranges added to each sheet")
 
-	// DO THIS TOMORROW - CHECK DB FOR USERS AND ADD THEM TO EACH SHEET
-
-	// put file id into db
+	// get users from db
 	dbcon, err := dbpool.Acquire(ctx)
 	if err != nil {
 		log.Fatal(err)
 		panic(err)
 	}
 	defer dbcon.Conn().Close(ctx)
-	if countIsGreaterThan0 {
-		truncateTable(dbcon, "schema.table")
-		if err != nil {
-			log.Fatal(err)
-			panic(err)
-		}
-	}
-	query := `insert into schema.table(file_id=$1)`
-	_, err = dbcon.Exec(
+	query := `
+		select
+			user_id,
+			user_name
+		from schema.table
+		order by user_name
+	`
+	rows, err := dbcon.Query(
 		ctx,
 		query,
-		fileID,
 	)
 	if err != nil {
 		log.Fatal(err)
 		panic(err)
 	}
+
+	users := []struct {
+		id   string
+		name string
+	}{}
+	for rows.Next() {
+		var userID string
+		var username string
+		err = rows.Scan(&userID, &username)
+		if err != nil {
+			log.Fatal(err)
+			panic(err)
+		}
+		user := struct {
+			id   string
+			name string
+		}{}
+		users = append(users, user)
+	}
+	// add users to each sheet
+	requests = []*sheets.Request{}
+	for sheetIndex, columnIndexMap := range columnMap.Mapping {
+		numColumns := len(columnIndexMap)
+		cellData := make([]*sheets.CellData, numColumns)
+		for columnIndex, columnData := range columnIndexMap {
+			for i := 0; i < len(users); i++ {
+				if columnIndex == 0 {
+					cellData[columnIndex] = &sheets.CellData{
+						UserEnteredValue: &sheets.ExtendedValue{
+							StringValue: &users[i].id,
+						},
+						UserEnteredFormat: columnData.ColumnFormat,
+					}
+				} else if columnIndex == 1 {
+					cellData[columnIndex] = &sheets.CellData{
+						UserEnteredValue: &sheets.ExtendedValue{
+							StringValue: &users[i].name,
+						},
+						UserEnteredFormat: columnData.ColumnFormat,
+					}
+				} else {
+					boolVal := false
+					cellData[columnIndex] = &sheets.CellData{
+						DataValidation: &sheets.DataValidationRule{
+							Condition: &sheets.BooleanCondition{
+								Type: "BOOLEAN",
+							},
+						},
+						UserEnteredValue: &sheets.ExtendedValue{
+							BoolValue: &boolVal,
+						},
+						UserEnteredFormat: columnData.ColumnFormat,
+					}
+				}
+			}
+		}
+		requests = append(requests, &sheets.Request{
+			AppendCells: &sheets.AppendCellsRequest{
+				Fields: "*",
+				Rows: []*sheets.RowData{
+					{
+						Values: cellData,
+					},
+				},
+				SheetId: int64(sheetMap[Expansion(sheetIndex)]),
+			},
+		})
+	}
+	_, err = gsheetsSvc.Spreadsheets.BatchUpdate(spreadsheet.SpreadsheetId, &sheets.BatchUpdateSpreadsheetRequest{
+		Requests: requests,
+	}).Do()
+	if err != nil {
+		log.Fatal(err)
+		panic(err)
+	}
+	log.Debug("users added to each sheet")
+
+	// delete the default sheet
+	var defaultSheetID int64 = 0
+	for i := 0; i < len(spreadsheet.Sheets); i++ {
+		isInSheetMap := false
+		for _, sheetID := range sheetMap {
+			if spreadsheet.Sheets[i].Properties.SheetId == int64(sheetID) {
+				isInSheetMap = true
+			}
+		}
+		if !isInSheetMap {
+			defaultSheetID = spreadsheet.Sheets[i].Properties.SheetId
+			break
+		}
+	}
+	_, err = gsheetsSvc.Spreadsheets.BatchUpdate(spreadsheet.SpreadsheetId, &sheets.BatchUpdateSpreadsheetRequest{
+		Requests: []*sheets.Request{
+			{
+				DeleteSheet: &sheets.DeleteSheetRequest{
+					SheetId: defaultSheetID,
+				},
+			},
+		},
+	}).Do()
+	if err != nil {
+		log.Fatal(err)
+		panic(err)
+	}
+	log.Debug("default sheet deleted")
+
+	// save what is needed to the db
+	queryBatch := &pgx.Batch{}
+	if countIsGreaterThan0 {
+		// truncate file id table
+		queryBatch.Queue(`truncate table schema.table`)
+		// truncate sheet id table
+		queryBatch.Queue(`truncate table schema.table`)
+	}
+	// put file id into db
+	queryBatch.Queue(`insert into schema.table(file_id=$1)`, fileID)
+	// put perm ids into db
+	for i := 0; i < len(permIDs); i++ {
+		queryBatch.Queue(`insert into schema.table(file_id=$1,permission_id=$2)`, fileID, permIDs[i])
+	}
+	// put sheet IDs into db
+	for exp, sheetID := range sheetMap {
+		sheetIDStr := strconv.FormatInt(int64(sheetID), 10)
+		queryBatch.Queue(`insert into schema.table(file_id=$1,expansion=$2,sheet_id=$3)`, fileID, int(exp), sheetIDStr)
+	}
+	_ = dbcon.SendBatch(ctx, queryBatch)
+	log.Debug("required data saved to db")
 }
 
 func onGuildReady(event *events.GuildReady) {
@@ -210,7 +347,9 @@ func onGuildReady(event *events.GuildReady) {
 
 	if count == 0 {
 		// create the file
-		log.Debug("file does not exist on startup")
+		log.Debug("file does not exist in db on startup")
+		buildFile(false)
+		log.Debug("file built")
 	} else if count == 1 {
 		// check if the file exists
 		var fileID string
@@ -232,6 +371,9 @@ func onGuildReady(event *events.GuildReady) {
 			// check if the file needs to be updated
 		} else {
 			// create the file
+			log.Debug("file exists in db on startup but does not exist in google drive")
+			buildFile(true)
+			log.Debug("file built")
 		}
 	} else {
 		err := fmt.Errorf("multiple files exist")
